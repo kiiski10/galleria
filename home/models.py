f414d11f63edcb3106c0609acf1e8b2b221725f9
PekkaL <pekka@levonoja.fi>
2024-09-28 02:53:02 +0300

    Begin project

---
+++ home/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
+
+class ImagePage(Page):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('image'),
+        FieldPanel('body'),
+    ]
+
