b6b03c8c72fbb6568eb9c2eb6b87216f3c925d23
PekkaL <pekka@levonoja.fi>
2024-11-30 12:00:26 +0200

    Fetch thumbnail images for embedded yotube videos

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
+
+from django.core.files.base import ContentFile
 from django.db import models
-from wagtail.models import Page
-from wagtail.fields import RichTextField
+from PIL import Image as PilImage
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+from wagtail.images.models import Image
+from wagtail.models import Page
+import io
+import requests
 
 
 class HomePage(Page):
@@ -32,4 +38,39 @@ class ImagePage(Page):
         FieldPanel('image'),
         FieldPanel('body'),
     ]
+
+    def get_youtube_video_id(self):
+        video_url = self.body.split("url=\"")[1].split("\"")[0]
+        video_id = video_url.split("/")[-1]
+        if "&" in video_id:
+            video_id = video_id.split("&")[0]
+        if "?t=" in video_id:
+            video_id = video_id.split("?t=")[0]
+        if "?v=" in video_id:
+            video_id = video_id.split("?v=")[1]
+        return video_id
+
+    def save(self, clean=True, user=None, log_action=False, **kwargs):
+        youtube_urls = [
+            "url=\"https://www.youtube",
+            "url=\"https://youtube",
+            "url=\"https://www.youtu.be",
+            "url=\"https://youtu.be",
+        ]
+        has_embedded_content = "<embed embedtype=\"media\"" in self.body
+        embedded_content_is_youtube_video = any(x in self.body for x in youtube_urls)
+
+        if has_embedded_content and embedded_content_is_youtube_video:
+            video_id = self.get_youtube_video_id()
+            thumbnail_url = f"http://img.youtube.com/vi/%s/0.jpg" % video_id
+            thumbnail_image = PilImage.open(requests.get(thumbnail_url, stream=True).raw)
+            image_io = io.BytesIO()
+            thumbnail_image.save(image_io, format='JPEG')
+            image_io.seek(0)
+            self.image = Image.objects.create(
+                title=self.title,
+                file=ContentFile(image_io.read(), name=f"{self.title}.jpg"),
+            )
+        super().save(clean, user, log_action, **kwargs)
+
 
