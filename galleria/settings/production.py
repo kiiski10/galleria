120a3d3e29247f87f5e99726a80610aba9513c6a
PekkaL <pekka@levonoja.fi>
2025-01-12 19:24:18 +0200

    Read sensitive setting variables from environment

--- galleria/settings/production.py
+++ galleria/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *
+import os
 
 DEBUG = False
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 try:
     from .local import *
